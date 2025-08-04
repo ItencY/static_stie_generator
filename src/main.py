9584d72a73eea90822ae2f78c074c536c6c1f841
ItencY <itency@outlook.com>
2025-08-04 12:42:47 +0300

    add some test case for TextNode class

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    res = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(res)
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
 main()
