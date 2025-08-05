4abd887dba5395e3894792066674e699f3c45451
ItencY <itency@outlook.com>
2025-08-05 11:10:33 +0300

    extract links from markdwon using regex

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_links,
+    extract_markdown_images,
 )
 
 from textnode import TextNode, TextType
@@ -9,11 +11,11 @@ from textnode import TextNode, TextType
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
         node = TextNode("This is text with a **bolded** word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
                 TextNode("This is text with a ", TextType.TEXT),
-                TextNode("bolded", TextType.BOLD_TEXT),
+                TextNode("bolded", TextType.BOLD),
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
@@ -23,13 +25,13 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode(
             "This is text with a **bolded** word and **another**", TextType.TEXT
         )
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
                 TextNode("This is text with a ", TextType.TEXT),
-                TextNode("bolded", TextType.BOLD_TEXT),
+                TextNode("bolded", TextType.BOLD),
                 TextNode(" word and ", TextType.TEXT),
-                TextNode("another", TextType.BOLD_TEXT),
+                TextNode("another", TextType.BOLD),
             ],
             new_nodes,
         )
@@ -38,24 +40,24 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode(
             "This is text with a **bolded word** and **another**", TextType.TEXT
         )
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
                 TextNode("This is text with a ", TextType.TEXT),
-                TextNode("bolded word", TextType.BOLD_TEXT),
+                TextNode("bolded word", TextType.BOLD),
                 TextNode(" and ", TextType.TEXT),
-                TextNode("another", TextType.BOLD_TEXT),
+                TextNode("another", TextType.BOLD),
             ],
             new_nodes,
         )
 
     def test_delim_italic(self):
         node = TextNode("This is text with an _italic_ word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC_TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
         self.assertListEqual(
             [
                 TextNode("This is text with an ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC_TEXT),
+                TextNode("italic", TextType.ITALIC),
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
@@ -63,13 +65,13 @@ class TestInlineMarkdown(unittest.TestCase):
 
     def test_delim_bold_and_italic(self):
         node = TextNode("**bold** and _italic_", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
-        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC_TEXT)
-        self.assertListEqual(
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        self.assertEqual(
             [
-                TextNode("bold", TextType.BOLD_TEXT),
+                TextNode("bold", TextType.BOLD),
                 TextNode(" and ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC_TEXT),
+                TextNode("italic", TextType.ITALIC),
             ],
             new_nodes,
         )
@@ -84,6 +86,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
         )
 
 
